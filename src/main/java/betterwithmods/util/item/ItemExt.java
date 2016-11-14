bc1d316f9794a8bdcba2ebadd24c0a53022c56d1
Koward <kiwyh723@gmail.com>
2016-11-14 15:38:30 +0100

    Set buoyancy value to more items.

--- src/main/java/betterwithmods/util/item/ItemExt.java
+++ src/main/java/betterwithmods/util/item/ItemExt.java
@@ -1,7 +1,12 @@
 package betterwithmods.util.item;
 
+import betterwithmods.BWMBlocks;
+import betterwithmods.BWMItems;
+import betterwithmods.blocks.BlockMechMachines;
+import betterwithmods.items.ItemMaterial;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
@@ -81,6 +86,21 @@ public final class ItemExt {
         buoyancy.put(Blocks.WOODEN_SLAB, 1.0F);
         buoyancy.put(Items.DYE, 1.0F);
         buoyancy.put(Blocks.WOODEN_BUTTON, 1.0F);
+        buoyancy.put(BWMBlocks.AXLE, 1.0F);
+        buoyancy.put(BWMBlocks.PUMP, 0.0F);
+        buoyancy.put(BWMBlocks.WOOD_SIDING, 1.0F);
+        buoyancy.put(BWMBlocks.WOOD_MOULDING, 1.0F);
+        buoyancy.put(BWMBlocks.WOOD_CORNER, 1.0F);
+        buoyancy.put(BWMBlocks.SINGLE_MACHINES, BlockMechMachines.EnumType.HOPPER.getMeta(), 1.0F);
+        buoyancy.put(BWMBlocks.SAW, 1.0F);
+        buoyancy.put(BWMBlocks.PLATFORM, 1.0F);
+        buoyancy.put(BWMBlocks.SINGLE_MACHINES, BlockMechMachines.EnumType.PULLEY.getMeta(), 1.0F);
+        buoyancy.put(BWMBlocks.WOLF, 1.0F);
+        buoyancy.put(BWMBlocks.HEMP, 1.0F);
+        buoyancy.put(BWMBlocks.ROPE, 1.0F);
+        buoyancy.put(BWMBlocks.GEARBOX, 1.0F);
+        buoyancy.put(BWMBlocks.BELLOWS, 1.0F);
+        buoyancy.put(BWMBlocks.VASE, 1.0F);
         buoyancy.put(Items.APPLE, 1.0F);
         buoyancy.put(Items.BOW, 1.0F);
         buoyancy.put(Items.ARROW, 1.0F);
@@ -149,8 +169,38 @@ public final class ItemExt {
         buoyancy.put(Items.SKULL, 1.0F);
         buoyancy.put(Items.CARROT_ON_A_STICK, 1.0F);
         buoyancy.put(Items.PUMPKIN_PIE, 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("hemp").getMetadata(), 1.0F);
+        buoyancy.put("gearWood", 1.0F);
+        buoyancy.put("foodFlour", 1.0F);
         buoyancy.put("string", 1.0F);
-        buoyancy.put("bark", 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("scoured_leather").getMetadata(), 1.0F);
+        buoyancy.put(BWMItems.DONUT, 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("dung").getMetadata(), 1.0F);
+        buoyancy.put(BWMItems.WINDMILL, 1.0F);
+        buoyancy.put("fabricHemp", 1.0F);
+        buoyancy.put(BWMItems.DONUT, 1.0F);
+        buoyancy.put(BWMItems.DONUT, 1.0F);
+        buoyancy.put(BWMBlocks.GRATE, 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("tanned_leather").getMetadata(), 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("leather_strap").getMetadata(), 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("leather_belt").getMetadata(), 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("wood_blade").getMetadata(), 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("glue").getMetadata(), 0.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("tallow").getMetadata(), 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("haft").getMetadata(), 1.0F);
+        buoyancy.put(BWMBlocks.URN, 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("sawdust").getMetadata(), 1.0F);
+        buoyancy.put(BWMItems.DYNAMITE, 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("soul_dust").getMetadata(), 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("nether_sludge").getMetadata(), 1.0F);
+        buoyancy.put(Items.CARROT, 1.0F);
+        buoyancy.put(BWMItems.CREEPER_OYSTER, 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("leather_cut").getMetadata(), 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("tanned_leather_cut").getMetadata(), 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("scoured_leather_cut").getMetadata(), 1.0F);
+        buoyancy.put("barkWood", 1.0F);
+        buoyancy.put(Items.DYE, EnumDyeColor.BROWN.getDyeDamage(), 1.0F);
+        buoyancy.put(BWMItems.MATERIAL, ItemMaterial.getMaterial("soul_flux").getMetadata(), 1.0F);
     }
 
     public static void initWeights() {

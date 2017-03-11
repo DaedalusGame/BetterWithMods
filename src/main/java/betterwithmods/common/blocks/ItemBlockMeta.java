dd6e51b0de148d9b14300de4d33474f38956e591
BeetoGuy <BeetoGuy@gmail.com>
2017-03-11 15:43:37 -0700

    Add contained item information to vases

--- src/main/java/betterwithmods/common/blocks/ItemBlockMeta.java
+++ src/main/java/betterwithmods/common/blocks/ItemBlockMeta.java
@@ -1,8 +1,15 @@
 package betterwithmods.common.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemBlockMeta extends ItemBlock {
     public ItemBlockMeta(Block block) {
@@ -18,5 +25,27 @@ public class ItemBlockMeta extends ItemBlock {
     @Override
     public String getUnlocalizedName(ItemStack stack) {
         return super.getUnlocalizedName() + "." + stack.getItemDamage();
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List<String> tooltip, boolean advanced) {
+        super.addInformation(stack, player, tooltip, advanced);
+
+        if (stack.hasTagCompound()) {
+            if (stack.getTagCompound().hasKey("Items")) {
+                NBTTagList list = stack.getTagCompound().getTagList("Items", 10);
+                List<String> items = new ArrayList<>();
+                items.add(I18n.format("bwm.item.containment"));
+                for (int i = 0; i < list.tagCount() && items.size() < 4; i++) {
+                    NBTTagCompound tag = list.getCompoundTagAt(i);
+                    ItemStack s = new ItemStack(tag);
+                    if (!s.isEmpty())
+                        items.add(s.getCount() + "x " + s.getDisplayName());
+                    if (items.size() == 4 && list.tagCount() > 3)
+                        items.add("...");
+                }
+                tooltip.addAll(items);
+            }
+        }
     }
 }

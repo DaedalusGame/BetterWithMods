cab869c2616906a2f7edc1c8e03cf6bbf09c371c
Tyler Marshall <primetoxinzz@gmail.com>
2017-05-09 22:32:26 -0400

    Improve HCOre implementation:  Separate Ore and Dust changes, add config to disable both. Add config to exclude materials from HCOre.

--- src/main/java/betterwithmods/module/hardcore/HCOres.java
+++ src/main/java/betterwithmods/module/hardcore/HCOres.java
@@ -11,13 +11,32 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
+import org.apache.commons.lang3.tuple.Pair;
+import scala.actors.threadpool.Arrays;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by tyler on 4/20/17.
  */
 public class HCOres extends Feature {
+
+    private static boolean oreNuggetSmelting, dustNuggetSmelting, fixVanillaRecipes;
+    private static StringList oreExclude, dustExclude;
+
+    @Override
+    public void setupConfig() {
+        oreNuggetSmelting = loadPropBool("Ore to Nugget Smelting", "Make Ores (oredict ore.* )smelt into nuggets instead of ingots", true);
+
+        oreExclude = StringList.asList(loadPropStringList("Ore Exclude", "Oredictionary entries to exclude from ore to nugget smelting. Remove the prefix of the oredictionary. example 'oreIron' would be just 'iron' ", new String[0]));
+        dustExclude = StringList.asList(loadPropStringList("Dust Exclude", "Oredictionary entries to exclude from dust to nugget smelting  Remove the prefix of the oredictionary. example 'dustIron' would be just 'iron'", new String[0]));
+
+        dustNuggetSmelting = loadPropBool("Dust to Nugget Smelting", "Make Dusts ( oredict dust.* ) smelt into nuggets instead of ingots", true);
+        fixVanillaRecipes = loadPropBool("Fix Vanilla Recipes", "Make certain recipes cheaper to be more reasonable with nugget smelting, including Compass, Clock, and Bucket", true);
+    }
+
     @Override
     public String getFeatureDescription() {
         return "Makes Ores only smelt into a single nugget, making it much harder to create large amounts of metal";
@@ -30,6 +49,7 @@ public class HCOres extends Feature {
 
     @Override
     public void init(FMLInitializationEvent event) {
+
         RecipeUtils.removeRecipes(Items.COMPASS, 0);
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.COMPASS), " N ", "NRN", " N ", 'N', "nuggetIron", 'R', "dustRedstone"));
         RecipeUtils.removeRecipes(Items.CLOCK, 0);
@@ -40,20 +60,52 @@ public class HCOres extends Feature {
 
     @Override
     public void postInit(FMLPostInitializationEvent event) {
-        for (ItemStack ore : InvUtils.oreNames) {
-            String suffix = InvUtils.getSuffix(ore, "ore");
-            if (suffix != null) {
-                RecipeUtils.removeFurnaceRecipe(ore);
-                ItemStack nugget = OreDictionary.getOres("nugget" + suffix).stream().findFirst().orElse(ItemStack.EMPTY);
-                if (!nugget.isEmpty()) {
-                    FurnaceRecipes.instance().getSmeltingList().put(ore,nugget);
-                    List<ItemStack> dusts = OreDictionary.getOres("dust"+suffix);
-                    if (dusts.size() > 0) {
-                        dusts.forEach(RecipeUtils::removeFurnaceRecipe);
-                        dusts.forEach(dust -> FurnaceRecipes.instance().getSmeltingList().put(dust, nugget));
+        if (oreNuggetSmelting) {
+            List<Pair<ItemStack, String>> oreSuffixes = InvUtils.oreNames.stream().map(i -> Pair.of(i, InvUtils.getSuffix(i, "ore"))).filter(p -> !oreExclude.contains(p.getValue())).collect(Collectors.toList());
+            oreSuffixes.forEach(pair -> OreDictionary.getOres("nugget" + pair.getValue()).stream().findFirst().ifPresent(nugget -> {
+                RecipeUtils.removeFurnaceRecipe(pair.getKey());
+                FurnaceRecipes.instance().getSmeltingList().put(pair.getKey(), nugget);
+            }));
+        }
+        if (dustNuggetSmelting) {
+            List<Pair<ItemStack, String>> dustSuffixes = InvUtils.dustNames.stream().map(i -> Pair.of(i, InvUtils.getSuffix(i, "dust"))).filter(p -> !dustExclude.contains(p.getValue())).collect(Collectors.toList());
+            dustSuffixes.forEach(pair -> OreDictionary.getOres("nugget" + pair.getValue()).stream().findFirst().ifPresent(nugget -> {
+                RecipeUtils.removeFurnaceRecipe(pair.getKey());
+                FurnaceRecipes.instance().getSmeltingList().put(pair.getKey(), nugget);
+            }));
+        }
+    }
+
+    private static class StringList extends ArrayList<String> {
+        public static StringList asList(String[] array) {
+            StringList list = new StringList();
+            list.addAll(Arrays.asList(array));
+            return list;
+        }
+
+        public boolean contains(String o) {
+            return indexOf(o) >= 0;
+        }
+
+
+        public int indexOf(String var1) {
+            int var2;
+            if(var1 == null) {
+                for(var2 = 0; var2 < this.size(); ++var2) {
+                    if(this.get(var2) == null) {
+                        return var2;
+                    }
+                }
+            } else {
+                for(var2 = 0; var2 < this.size(); ++var2) {
+                    if(var1.equalsIgnoreCase(this.get(var2))) {
+                        return var2;
                     }
                 }
             }
+            return -1;
         }
     }
+
+
 }

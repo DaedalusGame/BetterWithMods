db221ef51c541286e7a4ebbbf2981ef4064035e4
Tyler Marshall <primetoxinzz@gmail.com>
2017-05-11 16:38:56 -0400

    Fix hemp seed registration.

--- src/main/java/betterwithmods/module/tweaks/HempSeed.java
+++ src/main/java/betterwithmods/module/tweaks/HempSeed.java
@@ -4,7 +4,7 @@ import betterwithmods.common.BWMBlocks;
 import betterwithmods.module.Feature;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 /**
  * Created by tyler on 4/20/17.
@@ -16,7 +16,7 @@ public class HempSeed extends Feature {
     }
 
     @Override
-    public void preInit(FMLPreInitializationEvent event) {
-            MinecraftForge.addGrassSeed(new ItemStack(BWMBlocks.HEMP, 1, 0), 5);
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.addGrassSeed(new ItemStack(BWMBlocks.HEMP, 1, 0), 5);
     }
 }

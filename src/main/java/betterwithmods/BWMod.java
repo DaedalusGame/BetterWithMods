fdb6d465e3307c501fc525629a083943e1cf4aea
Tyler Marshall <primetoxinzz@gmail.com>
2017-05-11 16:42:24 -0400

    Bump Version.

--- src/main/java/betterwithmods/BWMod.java
+++ src/main/java/betterwithmods/BWMod.java
@@ -29,7 +29,7 @@ import java.util.*;
 @Mod(modid = BWMod.MODID, name = BWMod.NAME, version = BWMod.VERSION, dependencies = "before:survivalist;after:mantle;after:tconstruct;after:minechem;after:natura;after:terrafirmacraft;after:immersiveengineering;after:quark;after:mekanism;after:thermalexpansion", guiFactory = "betterwithmods.client.gui.BWGuiFactory")
 public class BWMod {
     public static final String MODID = "betterwithmods";
-    public static final String VERSION = "1.1.7";
+    public static final String VERSION = "1.1.8";
     public static final String NAME = "Better With Mods";
     private static final Set<ICompatModule> loadedModules = new HashSet<>();
 

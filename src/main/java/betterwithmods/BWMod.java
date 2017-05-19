36e9f47d3518d152008e31b7eeda10b3f3a8e3e7
BeetoGuy <BeetoGuy@gmail.com>
2017-05-19 11:30:52 -0700

    Fix #357

--- src/main/java/betterwithmods/BWMod.java
+++ src/main/java/betterwithmods/BWMod.java
@@ -32,9 +32,9 @@ import java.util.Set;
 @Mod(modid = BWMod.MODID, name = BWMod.NAME, version = BWMod.VERSION, dependencies = BWMod.DEPENDENCIES, guiFactory = "betterwithmods.client.gui.BWGuiFactory")
 public class BWMod {
     public static final String MODID = "betterwithmods";
-    public static final String VERSION = "1.2.7-1.11.2";
+    public static final String VERSION = "1.2.9-1.11.2";
     public static final String NAME = "Better With Mods";
-    public static final String DEPENDENCIES = "before:survivalist;after:mantle;after:tconstruct;after:minechem;after:natura;after:terrafirmacraft;after:immersiveengineering;after:quark;after:mekanism;after:thermalexpansion";
+    public static final String DEPENDENCIES = "before:survivalist;after:mantle;after:tconstruct;after:minechem;after:natura;after:terrafirmacraft;after:immersiveengineering;after:mekanism;after:thermalexpansion";
     private static final Set<ICompatModule> loadedModules = new HashSet<>();
 
     /**

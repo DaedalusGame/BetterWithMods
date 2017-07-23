bfbafaf25d415116ce43e93015fc2df6eed20b52
Tyler Marshall <primetoxinzz@gmail.com>
2017-07-23 10:44:01 -0400

    Fix HCStructures not generating.
    (cherry picked from commit 3bbb6b5)

--- src/main/java/betterwithmods/common/world/BWMapGenScatteredFeature.java
+++ src/main/java/betterwithmods/common/world/BWMapGenScatteredFeature.java
@@ -21,22 +21,17 @@ public class BWMapGenScatteredFeature extends MapGenScatteredFeature {
     public static HashMap<String, Set<Biome>> STRUCTURE_BIOMES = new HashMap<>();
 
     static {
-        STRUCTURE_BIOMES.put("WitchHut", Sets.newHashSet(Biomes.SWAMPLAND, Biomes.MUTATED_SWAMPLAND, Biomes.ROOFED_FOREST, Biomes.MUTATED_ROOFED_FOREST,Biomes.EXTREME_HILLS));
+        STRUCTURE_BIOMES.put("WitchHut", Sets.newHashSet(Biomes.SWAMPLAND, Biomes.MUTATED_SWAMPLAND, Biomes.ROOFED_FOREST, Biomes.MUTATED_ROOFED_FOREST, Biomes.EXTREME_HILLS));
         STRUCTURE_BIOMES.put("SandTemple", Sets.newHashSet(Biomes.DESERT, Biomes.DESERT_HILLS, Biomes.MUTATED_DESERT, Biomes.SAVANNA, Biomes.MESA));
         STRUCTURE_BIOMES.put("Igloo", Sets.newHashSet(Biomes.ICE_PLAINS, Biomes.MUTATED_ICE_FLATS, Biomes.ICE_MOUNTAINS, Biomes.COLD_TAIGA, Biomes.COLD_TAIGA_HILLS, Biomes.MUTATED_TAIGA_COLD));
-        STRUCTURE_BIOMES.put("StoneTemple", Sets.newHashSet(Biomes.JUNGLE, Biomes.JUNGLE_EDGE, Biomes.JUNGLE_HILLS, Biomes.MUTATED_JUNGLE, Biomes.MUTATED_JUNGLE_EDGE, Biomes.PLAINS, Biomes.MUTATED_REDWOOD_TAIGA,Biomes.FOREST));
-    }
-
-    @Override
-    protected StructureStart getStructureStart(int chunkX, int chunkZ) {
-        return new BWMapGenScatteredFeature.Start(this.world, this.rand, chunkX, chunkZ);
+        STRUCTURE_BIOMES.put("StoneTemple", Sets.newHashSet(Biomes.JUNGLE, Biomes.JUNGLE_EDGE, Biomes.JUNGLE_HILLS, Biomes.MUTATED_JUNGLE, Biomes.MUTATED_JUNGLE_EDGE, Biomes.PLAINS, Biomes.MUTATED_REDWOOD_TAIGA, Biomes.FOREST));
     }
 
     public static StructureComponent getComponent(World world, Random random, int chunkX, int chunkZ, Biome biome) {
-        if(GlobalConfig.debug)
-        System.out.printf("/tp %s ~ %s\n", chunkX*16, chunkZ*16);
+        if (GlobalConfig.debug)
+            System.out.printf("/tp %s ~ %s\n", chunkX * 16, chunkZ * 16);
         for (String structure : STRUCTURE_BIOMES.keySet()) {
-            if (STRUCTURE_BIOMES.containsValue(biome)) {
+            if (STRUCTURE_BIOMES.get(structure).contains(biome)) {
                 switch (structure) {
                     case "WitchHut":
                         return new BWComponentScatteredFeaturePieces.SwampHut(random, chunkX * 16, chunkZ * 16);
@@ -52,6 +47,11 @@ public class BWMapGenScatteredFeature extends MapGenScatteredFeature {
         return null;
     }
 
+    @Override
+    protected StructureStart getStructureStart(int chunkX, int chunkZ) {
+        return new BWMapGenScatteredFeature.Start(this.world, this.rand, chunkX, chunkZ);
+    }
+
     public static class Start extends MapGenScatteredFeature.Start {
         public Start() {
         }
@@ -61,8 +61,8 @@ public class BWMapGenScatteredFeature extends MapGenScatteredFeature {
         }
 
         Start(World worldIn, Random random, int chunkX, int chunkZ, Biome biomeIn) {
-            StructureComponent component = getComponent(worldIn,random,chunkX,chunkZ,biomeIn);
-            if(component != null)
+            StructureComponent component = getComponent(worldIn, random, chunkX, chunkZ, biomeIn);
+            if (component != null)
                 components.add(component);
             this.updateBoundingBox();
         }

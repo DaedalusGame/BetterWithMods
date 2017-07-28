54a5b1f5576ac5c90306a0c8047f4be96a8486b5
Tyler Marshall <primetoxinzz@gmail.com>
2017-07-27 21:23:54 -0400

    Fix HCSaw recipes.

--- src/main/java/betterwithmods/module/hardcore/HCSaw.java
+++ src/main/java/betterwithmods/module/hardcore/HCSaw.java
@@ -3,10 +3,8 @@ package betterwithmods.module.hardcore;
 import betterwithmods.common.BWMRecipes;
 import betterwithmods.module.Feature;
 import com.google.common.collect.Sets;
-import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
@@ -23,33 +21,31 @@ public class HCSaw extends Feature {
 
     @Override
     public void preInit(FMLPreInitializationEvent event) {
-        Set<Block> blocks = Sets.newHashSet(
-                Blocks.OAK_FENCE,
-                Blocks.ACACIA_FENCE,
-                Blocks.BIRCH_FENCE,
-                Blocks.SPRUCE_FENCE,
-                Blocks.DARK_OAK_FENCE,
-                Blocks.JUNGLE_FENCE,
-                Blocks.OAK_FENCE,
-                Blocks.OAK_FENCE_GATE,
-                Blocks.ACACIA_FENCE_GATE,
-                Blocks.BIRCH_FENCE_GATE,
-                Blocks.SPRUCE_FENCE_GATE,
-                Blocks.DARK_OAK_FENCE_GATE,
-                Blocks.JUNGLE_FENCE_GATE,
-                Blocks.OAK_FENCE_GATE,
-                Blocks.TRAPDOOR
+        Set<ItemStack> blocks = Sets.newHashSet(
+                new ItemStack(Blocks.OAK_FENCE),
+                new ItemStack(Blocks.ACACIA_FENCE),
+                new ItemStack(Blocks.BIRCH_FENCE),
+                new ItemStack(Blocks.SPRUCE_FENCE),
+                new ItemStack(Blocks.JUNGLE_FENCE),
+                new ItemStack(Blocks.OAK_FENCE),
+                new ItemStack(Blocks.DARK_OAK_FENCE),
+                new ItemStack(Blocks.OAK_FENCE_GATE),
+                new ItemStack(Blocks.BIRCH_FENCE_GATE),
+                new ItemStack(Blocks.SPRUCE_FENCE_GATE),
+                new ItemStack(Blocks.DARK_OAK_FENCE_GATE),
+                new ItemStack(Blocks.JUNGLE_FENCE_GATE),
+                new ItemStack(Blocks.OAK_FENCE_GATE),
+                new ItemStack(Blocks.ACACIA_FENCE_GATE),
+                new ItemStack(Blocks.TRAPDOOR),
+                new ItemStack(Items.OAK_DOOR),
+                new ItemStack(Items.ACACIA_DOOR),
+                new ItemStack(Items.BIRCH_DOOR),
+                new ItemStack(Items.SPRUCE_DOOR),
+                new ItemStack(Items.DARK_OAK_DOOR),
+                new ItemStack(Items.JUNGLE_DOOR)
 
         );
-        Set<Item> items = Sets.newHashSet(
-                Items.OAK_DOOR,
-                Items.ACACIA_DOOR,
-                Items.BIRCH_DOOR,
-                Items.SPRUCE_DOOR,
-                Items.DARK_OAK_DOOR,
-                Items.JUNGLE_DOOR);
-        blocks.stream().map(ItemStack::new).forEach(BWMRecipes::removeRecipe);
-        items.stream().map(ItemStack::new).forEach(BWMRecipes::removeRecipe);
+        blocks.forEach(BWMRecipes::removeRecipe);
     }
 
 }

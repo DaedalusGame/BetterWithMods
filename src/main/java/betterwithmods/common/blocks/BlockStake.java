3f5682a5f3718791ae6ed7a785c11439ef871c26
Tyler Marshall <primetoxinzz@gmail.com>
2017-07-30 13:18:54 -0400

    Add Stake.

--- src/main/java/betterwithmods/common/blocks/BlockStake.java
+++ src/main/java/betterwithmods/common/blocks/BlockStake.java
@@ -5,6 +5,8 @@ import betterwithmods.util.DirUtils;
 import betterwithmods.util.InvUtils;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
@@ -19,6 +21,10 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
+import static net.minecraft.util.EnumFacing.Axis.X;
+import static net.minecraft.util.EnumFacing.Axis.Z;
+
+
 /**
  * Created by tyler on 5/17/17.
  */
@@ -27,6 +33,7 @@ public class BlockStake extends BWMBlock {
 
     public BlockStake() {
         super(Material.WOOD);
+        setDefaultState(getDefaultState().withProperty(DirUtils.NORTH, false).withProperty(DirUtils.SOUTH, false).withProperty(DirUtils.WEST, false).withProperty(DirUtils.EAST, false).withProperty(DirUtils.UP, false).withProperty(DirUtils.DOWN, false));
     }
 
     @Override
@@ -61,7 +68,7 @@ public class BlockStake extends BWMBlock {
     @Override
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
         ItemStack stack = playerIn.getHeldItemMainhand();
-        if (side != state.getValue(DirUtils.FACING) && stack.isItemEqual(new ItemStack(Items.STRING)))
+        if (stack.isItemEqual(new ItemStack(Items.STRING)))
             return placeString(worldIn, pos, side.getOpposite(), stack);
         return false;
     }
@@ -70,7 +77,8 @@ public class BlockStake extends BWMBlock {
         int count = stack.getCount();
         int build = -1;
         for (int i = 1; i <= 64; i++) {
-            if (world.getBlockState(pos.offset(facing, i)).getBlock() instanceof BlockStake) {
+            IBlockState stake = world.getBlockState(pos.offset(facing, i));
+            if (stake.getBlock() instanceof BlockStake) {
                 build = i;
                 break;
             }
@@ -78,7 +86,8 @@ public class BlockStake extends BWMBlock {
         if (build > -1 && count >= (build - 1)) {
             stack.shrink(build);
             for (int i = 1; i < build; i++) {
-                world.setBlockState(pos.offset(facing, i), BWMBlocks.STAKE_STRING.getDefaultState());
+                if (world.getBlockState(pos.offset(facing, i)).getBlock() != BWMBlocks.STAKE_STRING)
+                    world.setBlockState(pos.offset(facing, i), BWMBlocks.STAKE_STRING.getDefaultState());
             }
             return true;
         }
@@ -106,19 +115,32 @@ public class BlockStake extends BWMBlock {
         }
     }
 
+    public static PropertyBool[] Z_ROTATE = new PropertyBool[]{DirUtils.SOUTH, DirUtils.NORTH, DirUtils.UP, DirUtils.DOWN, DirUtils.EAST, DirUtils.WEST};
+    public static PropertyBool[] X_ROTATE = new PropertyBool[]{DirUtils.SOUTH, DirUtils.NORTH, DirUtils.WEST, DirUtils.EAST, DirUtils.DOWN, DirUtils.UP};
 
     @Override
     public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
         IBlockState newState = state;
-        for (int i = 0; i < EnumFacing.VALUES.length; i++) {
-            newState = newState.withProperty(DirUtils.DIR_PROP[i], getDirection(worldIn, pos, EnumFacing.getFront(i).getOpposite()));
+        EnumFacing.Axis axis = state.getValue(DirUtils.FACING).getAxis();
+        if (axis == Z) {
+            for (int i = 0; i < EnumFacing.VALUES.length; i++) {
+                newState = newState.withProperty(Z_ROTATE[i], getDirection(worldIn, pos, EnumFacing.getFront(i)));
+            }
+        } else if (axis == X) {
+            for (int i = 0; i < EnumFacing.VALUES.length; i++) {
+                newState = newState.withProperty(X_ROTATE[i], getDirection(worldIn, pos, EnumFacing.getFront(i)));
+            }
+        } else {
+            for (int i = 0; i < EnumFacing.VALUES.length; i++) {
+                newState = newState.withProperty(DirUtils.DIR_PROP[i], getDirection(worldIn, pos, EnumFacing.getFront(i)));
+            }
         }
         return newState;
     }
 
     public static boolean getDirection(IBlockAccess world, BlockPos pos, EnumFacing facing) {
         Block block = world.getBlockState(pos.offset(facing)).getBlock();
-        return block instanceof BlockStakeString || block instanceof BlockStake;
+        return block instanceof BlockStakeString;
     }
 
     @Override
@@ -127,14 +149,42 @@ public class BlockStake extends BWMBlock {
     }
 
     @Override
+    public BlockFaceShape getBlockFaceShape(IBlockAccess p_193383_1_, IBlockState p_193383_2_, BlockPos p_193383_3_, EnumFacing p_193383_4_) {
+        return BlockFaceShape.UNDEFINED;
+    }
+
+    @Override
+    public boolean isSideSolid(IBlockState base_state, IBlockAccess world, BlockPos pos, EnumFacing side) {
+        return false;
+    }
+
+    @Override
+    public boolean canPlaceBlockOnSide(World worldIn, BlockPos pos, EnumFacing side) {
+        return worldIn.isSideSolid(pos.offset(side.getOpposite()),side);
+    }
+
+    @Override
     public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
-        worldIn.notifyNeighborsOfStateChange(pos,this,true);
-        for(EnumFacing facing: EnumFacing.VALUES) {
-            if(getDirection(worldIn,pos,facing)) {
-                InvUtils.ejectStackWithOffset(worldIn, pos.offset(facing), getItem(worldIn, pos, state));
-                worldIn.setBlockToAir(pos.offset(facing));
+        worldIn.notifyNeighborsOfStateChange(pos, this, true);
+        for (EnumFacing facing : EnumFacing.VALUES) {
+            Block block = worldIn.getBlockState(pos.offset(facing)).getBlock();
+            if (block instanceof BlockStakeString) {
+                ((BlockStakeString) block).drop(worldIn, pos.offset(facing));
             }
         }
         super.breakBlock(worldIn, pos, state);
+    }
+
+    public boolean isSupported(World world, BlockPos pos, IBlockState state) {
+        EnumFacing side = state.getValue(DirUtils.FACING);
+        return world.isSideSolid(pos.offset(side), side.getOpposite());
+    }
+
+    @Override
+    public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos) {
+        if(!isSupported(worldIn,pos,state)) {
+            InvUtils.ejectStackWithOffset(worldIn,pos,getItem(worldIn,pos,state));
+            worldIn.setBlockToAir(pos);
+        }
     }
 }
--- src/main/java/betterwithmods/common/blocks/BlockStakeString.java
+++ src/main/java/betterwithmods/common/blocks/BlockStakeString.java
@@ -4,22 +4,47 @@ import betterwithmods.util.DirUtils;
 import betterwithmods.util.InvUtils;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-import java.util.Random;
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class BlockStakeString extends BWMBlock {
     public BlockStakeString() {
         super(Material.CLOTH);
+        setDefaultState(getDefaultState().withProperty(DirUtils.NORTH, false).withProperty(DirUtils.SOUTH, false).withProperty(DirUtils.WEST, false).withProperty(DirUtils.EAST, false).withProperty(DirUtils.UP, false).withProperty(DirUtils.DOWN, false));
+    }
+
+    @Override
+    public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn, boolean p_185477_7_) {
+        super.addCollisionBoxToList(state, worldIn, pos, entityBox, collidingBoxes, entityIn, p_185477_7_);
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        state = getActualState(state, source, pos);
+        AxisAlignedBB box = new AxisAlignedBB(7 / 16d, 7 / 16d, 7 / 16d, 9 / 16d, 9 / 16d, 9 / 16d);
+        if (state.getValue(DirUtils.EAST))
+            box = box.union(new AxisAlignedBB(0, 7 / 16d, 7 / 16d, 0.5, 9 / 16d, 9 / 16d));
+        if (state.getValue(DirUtils.WEST))
+            box = box.union(new AxisAlignedBB(0.5, 7 / 16d, 7 / 16d, 1, 9 / 16d, 9 / 16d));
+        if (state.getValue(DirUtils.NORTH))
+            box = box.union(new AxisAlignedBB(7 / 16d, 7 / 16d, 0, 9 / 16d, 9 / 16d, 0.5));
+        if (state.getValue(DirUtils.SOUTH))
+            box = box.union(new AxisAlignedBB(7 / 16d, 7 / 16d, 0.5, 9 / 16d, 9 / 16d, 1));
+        return box;
     }
 
     public boolean isFullCube(IBlockState state) {
@@ -30,23 +55,6 @@ public class BlockStakeString extends BWMBlock {
         return false;
     }
 
-    @Override
-    public void neighborChanged(IBlockState state, World world, BlockPos pos, Block blockIn, BlockPos fromPos) {
-        world.scheduleBlockUpdate(pos,blockIn,1,5);
-    }
-
-    @Override
-    public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
-        for (int i = 0; i < DirUtils.DIR_PROP.length; i++) {
-            if (state.getValue(DirUtils.DIR_PROP[i])) {
-                if (!getDirection(world, pos, EnumFacing.getFront(i))) {
-                    InvUtils.ejectStackWithOffset(world, pos, getItem(world, pos, state));
-                    world.setBlockToAir(pos);
-                    break;
-                }
-            }
-        }
-    }
 
     @Override
     protected BlockStateContainer createBlockState() {
@@ -76,6 +84,7 @@ public class BlockStakeString extends BWMBlock {
     @Override
     public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
         IBlockState newState = state;
+
         for (int i = 0; i < EnumFacing.VALUES.length; i++) {
             newState = newState.withProperty(DirUtils.DIR_PROP[i], getDirection(worldIn, pos, EnumFacing.getFront(i)));
         }
@@ -91,4 +100,34 @@ public class BlockStakeString extends BWMBlock {
     public BlockRenderLayer getBlockLayer() {
         return BlockRenderLayer.CUTOUT_MIPPED;
     }
+
+    public void drop(World world, BlockPos pos) {
+        if (world.getBlockState(pos).getBlock() instanceof BlockStakeString) {
+            InvUtils.ejectStackWithOffset(world, pos, getItem(world, pos, world.getBlockState(pos)));
+            world.setBlockToAir(pos);
+        }
+    }
+
+    @Override
+    public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
+        state = state.getActualState(worldIn,pos);
+        for (int i = 0; i < DirUtils.DIR_PROP.length; i++) {
+            if (state.getValue(DirUtils.DIR_PROP[i])) {
+                drop(worldIn, pos.offset(EnumFacing.getFront(i)));
+            }
+        }
+        super.breakBlock(worldIn, pos, state);
+    }
+
+
+    @Override
+    public BlockFaceShape getBlockFaceShape(IBlockAccess p_193383_1_, IBlockState p_193383_2_, BlockPos p_193383_3_, EnumFacing p_193383_4_) {
+        return BlockFaceShape.UNDEFINED;
+    }
+
+    @Override
+    public boolean isSideSolid(IBlockState base_state, IBlockAccess world, BlockPos pos, EnumFacing side) {
+        return false;
+    }
+
 }
